27d24e947c3c049a1650e59ae76c24fd04a226de
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-11-18 19:09:41 -0800

    user

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" User module
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+class User(BaseModel, Base):
+    """ User class """
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
