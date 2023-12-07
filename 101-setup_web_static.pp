b52cd76736d2b0286de94d2584a9743becbac1e5
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-07 04:21:05 -0800

    task6

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,69 +1,93 @@
-# 101-setup_web_static.pp
+# AirBnB clone web server setup and configuration
+# SCRIPT INCOMPLETE. NEEDS SOME MORE THINKING---
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+}
 
 # Ensure the web_static directory exists
-file { '/data':
-  ensure => 'directory',
+-> file { '/data':
+  ensure  => 'directory'
 }
 
-file { '/data/web_static':
-  ensure => 'directory',
+-> file { '/data/web_static':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases':
-  ensure => 'directory',
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/shared':
-  ensure => 'directory',
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test':
-  ensure => 'directory',
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
 }
 
 # Create a fake HTML file
-file { '/data/web_static/releases/test/index.html':
-  content => '<html><head></head><body>Holberton School</body></html>',
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
 }
 
 # Create a symbolic link
-file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test',
-  force   => true,
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
 
-# Give ownership of the /data/ folder to the ubuntu user AND group
-file { '/data':
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
+# Ensure the web_static directory exists
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
+
+-> file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
 }
 
 # Update Nginx configuration
-file { '/etc/nginx/sites-available/default':
-  content => "server {
-                listen 80 default_server;
-                server_name _;
-                location /hbnb_static {
-                    alias /data/web_static/current;
-                }
-                location /redirect_me {
-                    rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-                }
-                error_page 404 /404.html;
-                location = /404.html {
-                    root /usr/share/nginx/html;
-                    internal;
-                }
-                add_header X-Served-By $HOSTNAME;
-            }",
-  notify  => Service['nginx'],
+-> file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
 }
 
 # Notify Nginx to restart
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+-> exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
